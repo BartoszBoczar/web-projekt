5581a0fcd898ca76f60dc78d53aa73cb52f39b0a
BartoszBoczar <bboczara2010@op.pl>
2020-04-30 19:13:49 +0200

    initalized bootstrap project

---
+++ src/main/java/com/web/cinemawebproject/CinemaWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.web.cinemawebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaWebProjectApplication.class, args);
+	}
+
+}
