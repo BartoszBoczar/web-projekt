1be3ae336d0342e9925206406db5ef831096067a
BartoszBoczar <bboczara2010@op.pl>
2020-05-10 22:22:57 +0200

    Added backend project

---
+++ backend-projekt-web/src/main/java/com/backendprojektweb/BackendProjektWebApplication.java
@@ -0,0 +1,13 @@
+package com.backendprojektweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjektWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjektWebApplication.class, args);
+	}
+
+}
---
+++ backend-projekt-web/src/test/java/com/backendprojektweb/BackendProjektWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendprojektweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjektWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
