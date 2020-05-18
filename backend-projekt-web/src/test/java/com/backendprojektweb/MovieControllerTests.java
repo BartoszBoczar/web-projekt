3340cbc083554cb9cedff53e0f2c14c9130a4b88
Michal <233146@student.pwr.edu.pl>
2020-05-18 11:54:19 +0200

    Modified unit test for movies controller

--- backend-projekt-web/src/test/java/com/backendprojektweb/MovieControllerTests.java
+++ backend-projekt-web/src/test/java/com/backendprojektweb/MovieControllerTests.java
@@ -2,17 +2,23 @@ package com.backendprojektweb;
 
 import com.backendprojektweb.model.Movie;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-@SpringBootTest
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MovieControllerTests {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    int serverPort;
 
     String body = "{\n" +
             "\t\"title\": \"This_is_a_test\",\n" +
@@ -23,36 +29,35 @@ class MovieControllerTests {
 
     @Test
     void testGetAllRequest() {
-        TestRestTemplate testRestTemplate = new TestRestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String>entity = new HttpEntity<>(headers);
-        ResponseEntity responseEntity = testRestTemplate.exchange("http://localhost:8080/movies",
+        ResponseEntity responseEntity = testRestTemplate.exchange("http://localhost:" + serverPort + "/movies",
                 HttpMethod.GET, entity, List.class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
 
     @Test
     void testGetIdRequest() {
-        TestRestTemplate testRestTemplate = new TestRestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String>entity = new HttpEntity<>(headers);
-        ResponseEntity responseEntity = testRestTemplate.exchange("http://localhost:8080/movies/1",
+        ResponseEntity<Movie> responseEntity = testRestTemplate.exchange("http://localhost:" + serverPort + "/movies/1",
                 HttpMethod.GET, entity, Movie.class);
         Movie movie = (Movie) responseEntity.getBody();
+        assert movie != null;
         assertEquals("Shrek", movie.getTitle());
     }
 
     @Test
     void testPostRequest() {
-        TestRestTemplate testRestTemplate = new TestRestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String>entity = new HttpEntity<>(body, headers);
-        ResponseEntity responseEntity = testRestTemplate.exchange("http://localhost:8080/movies",
+        ResponseEntity<Movie> responseEntity = testRestTemplate.exchange("http://localhost:" + serverPort + "/movies",
                 HttpMethod.POST, entity, Movie.class);
         Movie movie = (Movie) responseEntity.getBody();
+        assert movie != null;
         assertEquals("This_is_a_test", movie.getTitle());
     }
 }
